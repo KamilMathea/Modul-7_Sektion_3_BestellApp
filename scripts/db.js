7b6c214c2d7420fb445d14d7a89ea066c7655993
Kamil Mathea <kamilmathe@gmail.com>
2026-02-25 14:37:35 +0100

    myDishes array with objects in db.js added

---
+++ scripts/db.js
@@ -0,0 +1,95 @@
+let myDishes = [
+    {
+        "category": "Burger",
+        "categoryImg": "./assets/icons/burgerSection.svg",
+        "categoryHeadline": "Burger & Sandwiches",
+        "dishes": [
+            {
+                "name": "Veggie mushroom black burger",
+                "description": "Mixed green salad, Tomatoes, Edamame and Mushrooms",
+                "price": 16.90,
+                "img": "./assets/img/burgerVeggieMushroomBlack.jpg"
+            },
+            {
+                "name": "All meat burger",
+                "description": "Beef, Bacon, Dill pickles, Smoked cheese, Ketchup and BBQ souse",
+                "price": 15.90,
+                "img": "./assets/img/burgerAllMeat.jpg"
+            },
+            {
+                "name": "Beef red burger",
+                "description": "Beef, Cheese, Tomatoes and Onion",
+                "price": 14.90,
+                "img": "./assets/img/burgerBeefRed.jpg"
+            },
+            {
+                "name": "Big chicken burger",
+                "description": "Grilled chicken breast, Tomatoes, Lettuce, Red onions and Bell pepper",
+                "price": 15.90,
+                "img": "./assets/img/burgerBigChicken.jpg"
+            }
+        ]
+    },
+    {
+        "category": "Pizza",
+        "categoryImg": "./assets/icons/pizzaSection.svg",
+        "categoryHeadline": "Pizza ",
+        "dishes": [
+            {
+                "name": "Pizza Margherita",
+                "description": "Tomato slice, Mozzarella, Basil and Olive oil",
+                "price": 11.90,
+                "img": "./assets/img/pizzaMargherita.jpg"
+            },
+            {
+                "name": "Pizza Chorizo",
+                "description": "Tomato slice, Mozzarella, Chorizo and Olive oil",
+                "price": 13.90,
+                "img": "./assets/img/pizzaChorizo.jpg"
+            },
+            {
+                "name": "Pizza Funghi",
+                "description": "Red onion, Olives, Butter Mushrooms and Mozzarella",
+                "price": 12.90,
+                "img": "./assets/img/pizzaFunghi.jpg"
+            },
+            {
+                "name": "Pizza Quattro Formaggi with Chicken",
+                "description": "Chicken, Mozarella, Gorgonzola, Fontina and Parmigiano Reggiano",
+                "price": 15.90,
+                "img": "./assets/img/pizzaQuattroFormaggiWithChicken.jpg"
+            }
+        ]
+    },
+    {
+        "category": "Salad",
+        "categoryImg": "./assets/icons/saladSection.svg",
+        "categoryHeadline": "Salad",
+        "dishes": [
+            {
+                "name": "Warm beef arugula salad",
+                "description": "Beef, Arugula, Field salad, Greek feta, Cherry tomatoes, Sun-dried Tomatoes and Balsamic-vinegar dressing",
+                "price": 16.90,
+                "img": "./assets/img/saladWarmBeefArugula.jpg"
+            },
+            {
+                "name": "Mini green Salad",
+                "description": "Green salad, Cucumber, Carrots, Parsley and Radishes",
+                "price": 7.90,
+                "img": "./assets/img/saladMiniGreen.jpg"
+            },
+            {
+                "name": "Green Salad with sea food",
+                "description": "Mixed greens, Cherry tomatoes, Red onions, Mussels, Squid rings, Shrimp and Dijon mustard-lemon dressing with dill",
+                "price": 16.90,
+                "img": "./assets/img/saladGreenSaladWithSeaFood.jpg"
+            },
+            {
+                "name": "Vegan green salad with tofu",
+                "description": "Green salad, Cherry tomatoes, Cucumber, Baby spinach, Edamame, Radishes, Bittercress, Tofu and Peanuts",
+                "price": 14.90,
+                "img": "./assets/img/saladVeganGreenSaladWithTofu.jpg"
+            }
+        ]
+    }
+]
